a1e0ed6e7e81e581885f494f12aff30eac1f4a50
Jerome Leclanche <jerome@leclan.ch>
2018-10-27 01:10:23 +0300

    feat(steam): Implement a Steam provider

--- test_settings.py
+++ test_settings.py
@@ -118,6 +118,7 @@ INSTALLED_APPS = (
     'allauth.socialaccount.providers.soundcloud',
     'allauth.socialaccount.providers.spotify',
     'allauth.socialaccount.providers.stackexchange',
+    'allauth.socialaccount.providers.steam',
     'allauth.socialaccount.providers.stripe',
     'allauth.socialaccount.providers.telegram',
     'allauth.socialaccount.providers.trello',
