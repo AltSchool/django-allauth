3741662190dda23b75df3906f466719bc843df68
Biel <bielalgaida@gmail.com>
2019-06-27 22:22:40 +0200

    feat(socialaccount): Added Strava as OAuth2 provider

--- test_settings.py
+++ test_settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     'allauth.socialaccount.providers.spotify',
     'allauth.socialaccount.providers.stackexchange',
     'allauth.socialaccount.providers.steam',
+    'allauth.socialaccount.providers.strava',
     'allauth.socialaccount.providers.stripe',
     'allauth.socialaccount.providers.telegram',
     'allauth.socialaccount.providers.trello',
