dd83f16511fd7911f7498891f1af9d700d03ce3a
hummeltech <6109326+hummeltech@users.noreply.github.com>
2019-07-09 11:42:02 -0700

    feat(socialaccount): Added OpenStreetMap as OAuth provider
    * Added OpenStreetMap as OAuth provider
    
    * Fixed `E501 line too long`

--- test_settings.py
+++ test_settings.py
@@ -106,6 +106,7 @@ INSTALLED_APPS = (
     'allauth.socialaccount.providers.nextcloud',
     'allauth.socialaccount.providers.odnoklassniki',
     'allauth.socialaccount.providers.openid',
+    'allauth.socialaccount.providers.openstreetmap',
     'allauth.socialaccount.providers.orcid',
     'allauth.socialaccount.providers.patreon',
     'allauth.socialaccount.providers.paypal',
