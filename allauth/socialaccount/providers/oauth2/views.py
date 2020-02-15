6f64fe5bea5dfc9f95a827fe966d3c5617ae1928
Savinay Nangalia <savinay.ng@gmail.com>
2020-02-15 13:41:51 -0800

    fix py2 tests

--- allauth/socialaccount/providers/oauth2/views.py
+++ allauth/socialaccount/providers/oauth2/views.py
@@ -2,19 +2,14 @@ from __future__ import absolute_import
 
 from datetime import timedelta
 
-from allauth.compat import urlparse, urljoin
-
-from requests import RequestException
-from urlparse import urljoin
-from urlparse import urlparse
-
-
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.utils import timezone
+from requests import RequestException
 
 from allauth.account import app_settings
+from allauth.compat import urlparse, urljoin
 from allauth.exceptions import ImmediateHttpResponse
 from allauth.socialaccount import providers
 from allauth.socialaccount.helpers import (
