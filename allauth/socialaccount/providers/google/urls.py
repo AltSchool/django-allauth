3e75a29094991b8852d13b5a012290f7aab5f73a
Erik Brown <erik@altschool.com>
2020-03-04 14:39:07 -0800

    ALTOS-21997 fixes issue with adding adroid google callback urls

--- allauth/socialaccount/providers/google/urls.py
+++ allauth/socialaccount/providers/google/urls.py
@@ -1,14 +1,13 @@
-from django.conf.urls import patterns, url
+from django.urls import path, include
 from allauth.socialaccount.providers.oauth2.urls import default_urlpatterns
 
 from .provider import GoogleProvider
 from .views import oauth2_callback_android
 
 
-urlpatterns = default_urlpatterns(GoogleProvider)
-urlpatterns += [
-    url('^' + GoogleProvider.id + '/login/callback_android/$',
-        oauth2_callback_android,
-        name=GoogleProvider.id + '_callback_android'
-    )
-]
+android_path = path(
+    '%s/login/callback_android' % GoogleProvider.id,
+    oauth2_callback_android,
+    name=GoogleProvider.id + '_callback_android'
+)
+urlpatterns = default_urlpatterns(GoogleProvider, [android_path])
--- allauth/socialaccount/providers/oauth2/urls.py
+++ allauth/socialaccount/providers/oauth2/urls.py
@@ -5,27 +5,16 @@ from allauth.utils import import_attribute
 from allauth.account import app_settings
 from allauth.socialaccount.providers.oauth2.views import proxy_login_callback
 
-def default_urlpatterns(provider):
+def default_urlpatterns(provider, additional_paths=[]):
     login_view = import_attribute(
         provider.get_package() + '.views.oauth2_login')
     callback_view = import_attribute(
         provider.get_package() + '.views.oauth2_callback')
 
-    urlpatterns = [
+    urlpatterns = additional_paths + [
         path('login/', login_view, name=provider.id + "_login"),
         path('login/callback/', callback_view, name=provider.id + "_callback"),
     ]
-    if (
-        app_settings.LOGIN_PROXY_REDIRECT_WHITELIST or
-        app_settings.LOGIN_PROXY_REDIRECT_DOMAIN_WHITELIST
-    ):
-        urlpatterns.append(
-            url('^login/callback/proxy/$',
-                proxy_login_callback,
-                {'callback_view_name': provider.id + '_callback'},
-                name=provider.id + '_proxy'
-            )
-        )
 
     if (
         app_settings.LOGIN_PROXY_REDIRECT_WHITELIST or
